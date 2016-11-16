e817a5ea89285ae131ad35ba97a7bd4143fb3571
Dylan Rankin <drankin@bu.edu>
2016-11-16 01:57:00 +0100

    runeff update

--- python/runEff.py
+++ python/runEff.py
@@ -11,7 +11,8 @@ process.GlobalTag.globaltag = "80X_dataRun2_Prompt_ICHEP16JEC_v0"
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-       '/store/relval/CMSSW_8_1_0_pre9/RelValSingleElectronPt35Extended/GEN-SIM-RECO/PU25ns_81X_mcRun2_asymptotic_v2_2023tiltedPU35-v1/10000/FA5F7E7E-7055-E611-8095-0025905B856E.root'
+       #'/store/relval/CMSSW_8_1_0_pre9/RelValSingleElectronPt35Extended/GEN-SIM-RECO/PU25ns_81X_mcRun2_asymptotic_v2_2023tiltedPU35-v1/10000/FA5F7E7E-7055-E611-8095-0025905B856E.root'
+       'file:RelValTTbar_UPG_PU35.root'
     )
 )
 
@@ -22,22 +23,15 @@ process.ak4PFchsCorrectedJets   = cms.EDProducer('CorrectedPFJetProducer',
     correctors  = cms.VInputTag('ak4PFCHSL1FastL2L3Corrector')
 )
 
-process.load("PhysicsTools/JetMCAlgos/HadronAndPartonSelector_cfi")  
-process.load("PhysicsTools/JetMCAlgos/AK4PFJetsMCFlavourInfos_cfi")
-#process.ak4JetFlavourInfos.jets = cms.InputTag("ak4PFchsCorrectedJets")
-process.ak4JetFlavourInfos.jets = cms.InputTag("ak4PFJetsCHS")
-
 process.check = cms.EDAnalyzer("RECOAnalyzer",
     vertices = cms.InputTag("offlinePrimaryVertices"),
     #jets = cms.InputTag("ak4PFchsCorrectedJets"),
     jets = cms.InputTag("ak4PFJetsCHS"),
-    btags = cms.InputTag("pfCombinedInclusiveSecondaryVertexV2BJetTags"),
-    flavorinfo = cms.InputTag("ak4JetFlavourInfos"),
+    met = cms.InputTag("pfMet"),
     genparticles = cms.InputTag("genParticles"),
     electrons = cms.InputTag("gedGsfElectrons"),
     beamspot = cms.InputTag("offlineBeamSpot"),
     conversions = cms.InputTag("particleFlowEGamma"),
-    muons = cms.InputTag("muons"),
     debug = cms.bool(True)
 )
 
@@ -46,5 +40,5 @@ process.TFileService = cms.Service("TFileService",
     closeFileFast = cms.untracked.bool(True)
 )
 
-#process.p = cms.Path(process.ak4PFCHSL1FastL2L3CorrectorChain + process.ak4PFchsCorrectedJets + process.selectedHadronsAndPartons + process.ak4JetFlavourInfos + process.check)
-process.p = cms.Path(process.selectedHadronsAndPartons + process.ak4JetFlavourInfos + process.check)
+#process.p = cms.Path(process.ak4PFCHSL1FastL2L3CorrectorChain + process.ak4PFchsCorrectedJets + process.check)
+process.p = cms.Path(process.check)
